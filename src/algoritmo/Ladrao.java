d2ea08defc23aedfa6a9868740dbb412287dab2b
Luiz Miguel <79269031+LuizMiguel1905@users.noreply.github.com>
2022-10-18 00:57:17 -0300

    Update Ladrao.java

--- src/algoritmo/Ladrao.java
+++ src/algoritmo/Ladrao.java
@@ -1,9 +1,64 @@
 package algoritmo;
 
+import java.util.Random;
+
 public class Ladrao extends ProgramaLadrao {
 	
+	public boolean direita = false;
+	public  boolean esquerda = false;
+	public boolean cima = false;
+	public boolean baixo = false;
+	
+	
 	public int acao() {
-		return (int) (Math.random() * 5);
+		
+
+		
+		for(int i = 0; i < 24; i++){
+			
+			int inimigo = sensor.getVisaoIdentificacao()[i];
+			//Inimigo em sua visão
+			if(inimigo == 100 || inimigo == 110) {
+				
+				
+				
+				if(i > 11) {
+					//Se mover para baixo e direita
+					
+					int valor = new Random().nextInt(100) + 1;
+					
+					if(valor > 60) {
+						System.out.println("Baixo");
+						baixo = true;
+						return (2);
+					}
+					if(valor < 59) {
+						direita = true;
+						System.out.println("Direita");
+						return (3);
+					}
+				}
+				if(i < 12) {
+					//Se mover para cima e para esquerda
+					int valor = new Random().nextInt(100) + 1;
+					if(valor > 60) {
+						cima = true;
+						System.out.println("Cima");
+						return (1);
+					}
+					if(valor < 59) {
+						esquerda = false;
+						System.out.println("Esquerda");
+						return (4);
+					}
+				}
+
+			}
+		}
+		
+		
+			return (int) (Math.random()*5);
+
 	}
 
 }
